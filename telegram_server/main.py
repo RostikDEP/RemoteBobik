a21387f55a15d3f08ad0f365bddf1d521f3985e6
Stadnikov Rostyslav <stadnikov2000@gmail.com>
2025-01-20 19:10:43 +0200

    Start development: Telegram Bot

---
+++ telegram_server/main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+from aiogram import Dispatcher, Bot
+from handlers import *
+import asyncio
+import os
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+bot = Bot(TOKEN)
+dp:Dispatcher = Dispatcher()
+
+async def main():
+    #Реєстрація обробників
+    dp.include_router(start.router)
+    #--------------------------
+    print("Bot is running...")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
